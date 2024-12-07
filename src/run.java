05575ed32cb8b631db50914a15ba203a650092e5
Gabriel Justus Ramos <gabrielramos1@ufpr.br>
2024-12-06 23:51:38 -0300

    Update run.java

--- src/run.java
+++ src/run.java
@@ -76,7 +76,7 @@ public class run extends Application {
             System.out.println("Jogador " + (currentPlayer + 1) + " está jogando.");
             detectEnterOrEsc(scene);
             if (lastKeyPressed.equals("ENTER")) {
-                board.gamers[currentPlayer].move
+                board.gamers[currentPlayer].move(board.map.properties(board.gamers[currentPlayer].position)
             }
             // Implementar a lógica do jogador
 
