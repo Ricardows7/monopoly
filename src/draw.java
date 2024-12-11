d81d0fa8ce0b9d3d8fd808b51092b83a18d08997
Gabriel Justus Ramos <gabrielramos1@ufpr.br>
2024-12-10 21:46:30 -0300

    Update draw.java

--- src/draw.java
+++ src/draw.java
@@ -529,7 +529,7 @@ public class draw extends Application {
                                             if (land instanceof special) {
                                                 special spec = (special) land;
                                                 if (spec.getCategory() == 5) {
-                                                    String cardText = spec.getEffect(spec.getNews().getCurrentCard());
+                                                    String cardText = spec.getEffect(spec.getNews().getCurCard());
                                             }
     
                                             movePlayer(gamer, lastPos, gamer.getSpecialDistance());
